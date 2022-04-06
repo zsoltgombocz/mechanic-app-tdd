d638ad75a44df52f88a3c3fd8b68fa7e7a82be5c
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-04-06 23:19:23 +0200

    added some logic to navbar when routing

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -17,18 +17,32 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link {{ Request::path() == 'mechanics' ? 'active' : '' }}" href="/mechanics">
+                <a class="nav-link" data-bs-target="#mechanics-nav" data-bs-toggle="collapse" href="#">
                     <i class="bi bi-people"></i>
                     <span>Szerelők kezelése</span>
+                    <i class="bi bi-chevron-down ms-auto"></i>
                 </a>
+                <ul id="mechanics-nav" class="nav-content collapse {{ Request::path() == 'mechanics' ? 'show' : '' }}"
+                    data-bs-parent="#sidebar-nav">
+                    <li>
+                        <a href="/mechanics" class="{{ Request::path() == 'mechanics' ? 'active' : '' }}">
+                            <i class="bi bi-circle"></i><span>Szerelők listázása</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="components-accordion.html">
+                            <i class="bi bi-circle"></i><span>Szerelők hozzáadása</span>
+                        </a>
+                    </li>
+                </ul>
             </li>
         @endif
 
         <li class="nav-item">
-            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+            <a class="nav-link collapsed" data-bs-target="#workseehts-nav" data-bs-toggle="collapse" href="#">
                 <i class="bi bi-clipboard"></i><span>Munkalapok</span><i class="bi bi-chevron-down ms-auto"></i>
             </a>
-            <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+            <ul id="workseehts-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
                 <li>
                     <a href="components-alerts.html">
                         <i class="bi bi-circle"></i><span>Munkalap 1</span>
