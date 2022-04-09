3ce0f0048e6b9cddfe2bf0cd4aab2b0226f88065
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-04-09 22:58:24 +0200

    added basic relation to get creator and assigned mechanic of a worksheet

--- app/Models/Worksheet.php
+++ app/Models/Worksheet.php
@@ -7,5 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Worksheet extends Model
 {
+    protected $fillable = ['admin_id'];
+
+    public function creator()
+    {
+        return $this->hasOne(User::class, 'id', 'admin_id');
+    }
+
+    public function mechanic()
+    {
+        return $this->hasOne(User::class, 'id', 'mechanic_id');
+    }
     use HasFactory;
 }
