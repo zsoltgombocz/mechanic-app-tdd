f3abd68b288bbee57cd6ab35ae7be2b29468b414
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-04-09 22:59:15 +0200

    massive fixes to be able to make an empty munkalap

--- database/migrations/2022_04_06_095436_create_worksheets_table.php
+++ database/migrations/2022_04_06_095436_create_worksheets_table.php
@@ -15,20 +15,20 @@ class CreateWorksheetsTable extends Migration
     {
         Schema::create('worksheets', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('admin_id');
+            $table->unsignedBigInteger('admin_id')->nullable()->default(NULL);
             $table->timestamps();
-            $table->string('customer_name');
-            $table->string('customer_addr');
-            $table->string('vehicle_license');
-            $table->string('vehicle_brand');
-            $table->string('vehicle_model');
-            $table->unsignedBigInteger('mechanic_id');
-            $table->tinyInteger('closed');
+            $table->string('customer_name')->nullable()->default(NULL);
+            $table->string('customer_addr')->nullable()->default(NULL);
+            $table->string('vehicle_license')->nullable()->default(NULL);
+            $table->string('vehicle_brand')->nullable()->default(NULL);
+            $table->string('vehicle_model')->nullable()->default(NULL);
+            $table->unsignedBigInteger('mechanic_id')->nullable()->default(NULL);
+            $table->tinyInteger('closed')->default('0');
             $table->dateTime('closed_at')->nullable()->default(NULL);
-            $table->tinyInteger('payment');
+            $table->tinyInteger('payment')->nullable()->default(NULL);
 
-            $table->foreign('admin_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('mechanic_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('admin_id')->references('id')->on('users')->onDelete('set null');
+            $table->foreign('mechanic_id')->references('id')->on('users')->onDelete('set null');
         });
     }
 
