2b9db6487dfce5f7c845a91c5cf2de2acf9719f6
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-04-09 23:00:25 +0200

    added middlewares to every route, added more routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\DatabaseRouteController;
 use App\Http\Controllers\MainPageController;
 use App\Http\Controllers\MechanicController;
 use Illuminate\Support\Facades\Route;
@@ -16,11 +17,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [MainPageController::class, 'index'])->middleware('auth')->middleware('injectdata')->name('Kezdőlap');
-Route::get('/mechanics', [MechanicController::class, 'index'])->middleware('auth')->middleware('injectdata')->name('Szerelők');
+Route::middleware(['checkdb', 'auth', 'getworksheets'])->group(function () {
+    Route::get('/', [MainPageController::class, 'index'])->name('Kezdőlap');
+    Route::get('/mechanics', [MechanicController::class, 'index'])->name('Szerelők');
+    Route::get('/mechanics/create', [MechanicController::class, 'create'])->name('Szerelő hozzáadása');
+    Route::post('/mechanics/create', [MechanicController::class, 'store']);
+    Route::get('/mechanics/{id}', [MechanicController::class, 'edit'])->name('Szerelő szerkesztése - ');
+    Route::post('/mechanics/update/{id}', [MechanicController::class, 'update']);
+    Route::post('/mechanics/delete/{id}', [MechanicController::class, 'destroy']);
+    Route::post('/logout', [LoginController::class, 'destroy']);
+});
 
+Route::middleware(['checkdb', 'guest'])->group(function () {
+    Route::get('/login', [LoginController::class, 'create'])->name('login');
+    Route::post('/login', [LoginController::class, 'store']);
+});
 
-Route::get('/login', [LoginController::class, 'create'])->middleware('guest')->name('login');
-Route::post('/login', [LoginController::class, 'store'])->middleware('guest');
-
-Route::post('/logout', [LoginController::class, 'destroy'])->middleware('auth');
+Route::get('/dbstatus', [DatabaseRouteController::class, 'index'])->name('Adatbázis kapcsolat');
