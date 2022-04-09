cd880c8979b187d0b0d6b3b031159fda68ab635a
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-04-09 22:54:31 +0200

    Added to every function auth check, added create, update, delete funcs

--- app/Http/Controllers/MechanicController.php
+++ app/Http/Controllers/MechanicController.php
@@ -6,6 +6,7 @@ use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class MechanicController extends Controller
 {
@@ -21,7 +22,7 @@ class MechanicController extends Controller
 
             return view('pages.mechanics', ['users' => $users]);
         } else {
-            return view('login');
+            return redirect('/login');
         }
     }
 
@@ -32,7 +33,11 @@ class MechanicController extends Controller
      */
     public function create()
     {
-        //
+        if (Auth::check() && Auth::user()->role_id == 1) {
+            return view('pages.mechanics_create');
+        } else {
+            return redirect('/');
+        }
     }
 
     /**
@@ -43,7 +48,31 @@ class MechanicController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        if (Auth::check() && Auth::user()->role_id == 1) {
+            $validation = $request->validate([
+                'login_id' => 'required|min:5|unique:users',
+                'name' => 'required',
+                'password' => 'required|min:5'
+            ]);
+
+            if (!$validation) {
+                return redirect()->back()->withInput([$request->only('login_id'), $request->only('name'), $request->only('role_id')]);
+            }
+
+            User::create([
+                'login_id' => $request->login_id,
+                'name' => $request->name,
+                'password' => Hash::make($request->password),
+                'role_id' => $request->role_id
+            ]);
+
+            return redirect()->intended('mechanics')->with(['alert' => [
+                'type' => 'success',
+                'message' => 'Szerelő létrehozva!'
+            ]]);
+        } else {
+            return redirect('/');
+        }
     }
 
     /**
@@ -54,7 +83,6 @@ class MechanicController extends Controller
      */
     public function show($id)
     {
-        //
     }
 
     /**
@@ -65,7 +93,21 @@ class MechanicController extends Controller
      */
     public function edit($id)
     {
-        //
+        if (Auth::check() && Auth::user()->role_id == 1) {
+            $mechanic = User::where('id', $id)->get()->first();
+            if ($mechanic === null) {
+                return redirect('mechanics')->with(['alert' => ['type' => 'danger', 'message' => 'Nem létezik ez a szerelő!']]);
+            }
+
+            return view('pages.mechanics_edit', [
+                'user' => $mechanic,
+                'extendRouteName' => [
+                    'id' => $mechanic['id']
+                ]
+            ]);
+        } else {
+            return redirect('/');
+        }
     }
 
     /**
@@ -77,7 +119,27 @@ class MechanicController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        if (Auth::check() && Auth::user()->role_id == 1) {
+            $validation = $request->validate([
+                'login_id' => 'required|min:5|unique:users,id,' . $id,
+                'name' => 'required',
+            ]);
+
+            if (!$validation) {
+                return redirect()->back()->withInput([$request->only('login_id'), $request->only('name'), $request->only('role_id')]);
+            }
+
+            User::find($id)->update([
+                'login_id' => $request->login_id,
+                'name' => $request->name,
+                'role_id' => $request->role_id,
+                'remember_token' => NULL
+            ]);
+
+            return redirect('mechanics')->with(['alert' => ['type' => 'success', 'message' => 'Sikeres mentés!']]);
+        } else {
+            return redirect('/');
+        }
     }
 
     /**
@@ -88,6 +150,13 @@ class MechanicController extends Controller
      */
     public function destroy($id)
     {
-        //
+        if (Auth::check() && Auth::user()->role_id == 1) {
+            if ($id === Auth::user()->id) return redirect('mechanics');
+
+            User::find($id)->delete();
+            return redirect('mechanics')->with(['alert' => ['type' => 'success', 'message' => 'Sikeres törlés!']]);
+        } else {
+            return redirect('/');
+        }
     }
 }
