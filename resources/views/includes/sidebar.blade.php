a77de54f329e24b86ae4429b0e931d47ffc8adc4
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-04-11 23:29:17 +0200

    fixed active class on specific navbar elemenets

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -11,12 +11,6 @@
         @endif
         @if (auth()->user()->role_id == 1)
             <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <i class="bi bi-clipboard-plus"></i>
-                    <span>Munkalap létrehozás</span>
-                </a>
-            </li>
-            <li class="nav-item">
                 <a class="nav-link" data-bs-target="#mechanics-nav" data-bs-toggle="collapse" href="#">
                     <i class="bi bi-people"></i>
                     <span>Szerelők kezelése</span>
@@ -26,13 +20,16 @@
                     class="nav-content collapse {{ Request::segment('1') == 'mechanics' ? 'show' : '' }}"
                     data-bs-parent="#sidebar-nav">
                     <li>
-                        <a href="/mechanics" class="{{ Request::segment('1') == 'mechanics' ? 'active' : '' }}">
+                        <a href="/mechanics"
+                            class="{{ Request::segment('1') == 'mechanics' && !Request::segment('2') ? 'active' : '' }}">
                             <i class="bi bi-circle"></i><span>Szerelők listázása</span>
                         </a>
                     </li>
                     <li>
-                        <a href="/mechanics/create">
-                            <i class="bi bi-circle"></i><span>Szerelők hozzáadása</span>
+                        <a href="/mechanics/create"
+                            class="{{ Request::segment('1') == 'mechanics' && Request::segment('2') == 'create' ? 'active' : '' }}">
+                            <i class="
+                            bi bi-circle"></i><span>Szerelők hozzáadása</span>
                         </a>
                     </li>
                 </ul>
@@ -43,12 +40,32 @@
             <a class="nav-link collapsed" data-bs-target="#workseehts-nav" data-bs-toggle="collapse" href="#">
                 <i class="bi bi-clipboard"></i><span>Munkalapok</span><i class="bi bi-chevron-down ms-auto"></i>
             </a>
-            <ul id="workseehts-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+            <ul id="workseehts-nav"
+                class="nav-content collapse {{ Request::segment('1') == 'worksheets' ? 'show' : '' }} "
+                data-bs-parent="#sidebar-nav">
+                <li>
+                    <a href="/worksheets"
+                        class="{{ Request::segment('1') == 'worksheets' && !Request::segment('2') ? 'active' : '' }}">
+                        <i class="bi bi-circle"></i><span>Összes munkalap</span>
+                    </a>
+                    @if (auth()->user()->role_id === 1)
+                        <a href="/worksheets/create"
+                            class="{{ Request::segment('1') == 'worksheets' && Request::segment('2') == 'create' ? 'active' : '' }}">
+                            <i class="bi bi-circle"></i>
+                            <span>Munkalap létrehozás</span>
+                        </a>
+                    @endif
+                </li>
+                <hr class="dropdown-divider">
+                <li class="nav-heading">Hozzád rendelve:</li>
+                @if (count(session()->get('assigned_worksheets')) === 0)
+                    <div class="text-danger text-center w-100">Nincs munkalap!</div>
+                @endif
                 @foreach (session()->get('assigned_worksheets') as $ws)
                     <li>
                         <a href="/worksheets/{{ $ws->id }}">
                             <i
-                                class="bi bi-circle"></i><span>{{ isset($ws->customer_name) ? $ws->customer_name . ' - ' . ws->id : 'Munkalap - ' . $ws->id }}</span>
+                                class="bi bi-circle"></i><span>{{ isset($ws->customer_name) ? $ws->customer_name . ' - ' . $ws->id : 'Munkalap - ' . $ws->id }}</span>
                         </a>
                     </li>
                 @endforeach
@@ -59,7 +76,7 @@
                         <li>
                             <a href="/worksheets/{{ $ws->id }}">
                                 <i
-                                    class="bi bi-circle"></i><span>{{ isset($ws->customer_name) ? $ws->customer_name . ' - ' . ws->id : 'Munkalap - ' . $ws->id }}</span>
+                                    class="bi bi-circle"></i><span>{{ isset($ws->customer_name) ? $ws->customer_name . ' - ' . $ws->id : 'Munkalap - ' . $ws->id }}</span>
                             </a>
                         </li>
                     @endforeach
