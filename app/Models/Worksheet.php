ad135dc183b8deb742314d4596a874c5140c842a
Gömböcz Zsolt <zsolt.gombocz00@gmail.com>
2022-04-13 17:09:23 +0200

    added fillable

--- app/Models/Worksheet.php
+++ app/Models/Worksheet.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Worksheet extends Model
 {
-    protected $fillable = ['admin_id'];
+    protected $fillable = ['mechanic_id', 'admin_id', 'customer_name', 'customer_addr', 'vehicle_license', 'vehicle_brand', 'vehicle_model', 'closed', 'payment', 'closed_at'];
 
     public function creator()
     {
